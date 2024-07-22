f242949c059432d4e94816a73215dc2259a56656
浅墨忆 <70341992+snove520@users.noreply.github.com>
2024-07-22 21:26:47 +0800

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,4 +1,5 @@
-wget -O kuma.zip https://github.com/louislam/uptime-kuma/archive/refs/tags/1.21.3.zip && unzip kuma.zip
-mv -b uptime-kuma-1.21.3/* ./ && mv -b uptime-kuma-1.21.3/.[^.]* ./ && rm -rf *~ && rm -rf uptime-kuma-1.21.3 && rm -rf README.md
+latest_version=$(curl -s https://api.github.com/repos/louislam/uptime-kuma/releases/latest | grep '"tag_name":' | cut -d '"' -f 4)
+wget -O kuma.zip https://github.com/louislam/uptime-kuma/archive/refs/tags/${latest_version}.zip && unzip kuma.zip
+mv -b uptime-kuma-${latest_version}/* ./ && mv -b uptime-kuma-${latest_version}/.[^.]* ./ && rm -rf *~ && rm -rf uptime-kuma-${latest_version} && rm -rf README.md
 npm ci --production && npm run download-dist
 echo 'require("./server/server.js");' > index.js
